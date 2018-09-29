333a71253316c9310bc85be80adbe2a6a996b506
Jenn <jswan02@gmail.com>
2018-09-29 11:30:31 -0500

    fixed dog pics

--- src/components/DogCard/DogCard.js
+++ src/components/DogCard/DogCard.js
@@ -7,7 +7,7 @@ const DogCard = props => (
   value={props.id}
   onClick={() => props.handleClick(props.id)}>
     <div className="img-container">
-      <img alt={props.name} src={props.image}/>
+      <img class = "imgclass" alt={props.name} src={props.image}/>
     </div>
   </div>
 );
